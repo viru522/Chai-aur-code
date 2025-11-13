5d0f8690244f31651b9a0d943df9c2209c55773d
viru522 <56133577+viru522@users.noreply.github.com>
2025-11-13 13:31:31 +0000

    ConversionOperation

---
+++ JavaScript/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score = "virendra";
+// let score = null;
+let score = true;
+//  console.log(score);
+
+console.log(typeof score);
+console.log(typeof(score));
+
+ let valueInNumber =Number (score);
+ console.log(typeof(valueInNumber));
+ console.log(valueInNumber);
+ 
+ // "33" => 33
+ // "33acb" => NaN
+ // true => 1; false => 0
+
+ let isLoggedIn = 1
+ let booleanInLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanInLoggedIn);
+ 
+// 1 => true; 0 => false;
+// "" => false;
+// "anyValue" => true;
+
+
+let someNumber = 33;
+let StringNumber = String(someNumber);
+console.log(StringNumber);
+console.log(typeof(StringNumber));
+
