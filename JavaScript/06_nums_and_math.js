a6875150a2f5cbc92cffbca100ca5e0275f854eb
viru522 <56133577+viru522@users.noreply.github.com>
2025-11-14 11:12:53 +0000

    numbers and math function

---
+++ JavaScript/06_nums_and_math.js
@@ -0,0 +1,41 @@
+//  const score = 400;
+//  console.log(score);
+ 
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNUmber = 123.5941;
+// console.log(otherNUmber.toPrecision(3));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+ //+++++++++++ MATH ++++++++++++++++++++++++++++++
+
+//  console.log(Math);
+//  console.log(Math.abs(-10)); // 10 it will make the number positive
+//  console.log(Math.round(4.59)); // it will roundof the value like 5
+//  console.log(Math.ceil(4.59)); // it will also round the value like 4.1 --> 5
+//  console.log(Math.floor(4.59)); // it will also round the value like 4.59 --> 4
+//  console.log(Math.random()); // it select the random value form 0 to 1 
+// console.log(Math.min(2,54,20,53,10)); //2
+// console.log(Math.max(2,54,20,53,10)); // 54
+
+ 
+//  console.log(Math.random()); 
+//  console.log(Math.round(Math.random()*10 +1)); 
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max - min +1))+min);
+
+ 
+ 
+ 
+ 
+
