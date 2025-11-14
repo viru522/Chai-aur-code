522f1e6e688099d04d664c644548090ab5a360f7
viru522 <56133577+viru522@users.noreply.github.com>
2025-11-14 06:11:04 +0000

    DataType- summary

---
+++ JavaScript/Datatypes-summary.js
@@ -0,0 +1,37 @@
+// primitive 
+
+// 7 types : String, Number, Boolearn, null, undefined, Symbol,BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+const isLoggedIn =false;
+const outsideTemp =null;
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123');
+
+
+const BigNumber =12345690098n
+
+console.log(BigNumber);
+console.log(typeof(BigNumber));
+
+
+console.log(id === anotherId);
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name : "virendra",
+    age : 24.5
+}
+
+const myfunction = function(){
+    console.log("hello");
+    
+}
+console.log(typeof(myfunction));
