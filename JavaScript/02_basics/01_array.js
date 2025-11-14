58009d99a34fb2daa79e61d20e1c551c5b6be991
viru522 <56133577+viru522@users.noreply.github.com>
2025-11-14 13:03:34 +0000

    new

---
+++ JavaScript/02_basics/01_array.js
@@ -0,0 +1,45 @@
+// array
+
+const myArr =[0,1,2,3,4]
+// const myHeros = ["shakitiman", "naagraj"]
+
+//  const myArr2 = new Array(1,2,3,4)
+//  console.log(myArr[1]);
+ 
+//  // Array methods
+
+//   //addition 
+// myArr.unshift(-1) // this will add in start
+
+// myArr.push(5);// this will add in end 
+// console.log(myArr); 
+
+// //Remove
+
+// myArr.shift() //this will remove in start
+// console.log(myArr); 
+
+// myArr.pop();  //this will remove in end
+// console.log(myArr);
+
+// join
+// const newArr = myArr.join();
+// console.log(myArr);
+// console.log(newArr); // it will convert the arr in to string
+// console.log(typeof(newArr)); // string
+
+// Slice, Splice
+
+console.log("A", myArr);
+const myn1 = myArr.slice(1,3) // by using function it will delete for current array
+console.log(myn1);
+
+const myn2 = myArr.splice(1,3) // by using function it will del for main array 
+console.log(myn2); 
+
+console.log(myArr);
+
+
+ 
+
+  
