51337c1eb118692bec44557412441ea5818ff1c8
viru522 <56133577+viru522@users.noreply.github.com>
2025-11-21 14:39:39 +0000

    for loop, break, continue

--- JavaScript/05_iterations/one.js
+++ JavaScript/05_iterations/one.js
@@ -7,13 +7,47 @@
 //     }
 //     console.log(element);
 // }
+// this is a time table
 
-for (let index = 1; index <=100; index++) {
-    const element = index;
-    for (let i = 1; i <= 10; i++) {
-        console.log(`${index} * ${i} = ${index*i}`);
-        const element = i;
-    }
-    console.log(index);
+// for (let index = 1; index <=100; index++) {
+//     const element = index;
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(`${index} * ${i} = ${index*i}`);
+//         const element = i;
+//     }
+//     console.log(index);
+// }
+
+ // Array 
+MyArray =["flash","batman","superman"]
+for (let index = 0; index < MyArray.length; index++) {
+    const element = MyArray[index];
+    console.log(element);
+    
     
 }
+
+
+//Break & Contiune
+// continue
+for (let i = 0; i <= 20; i++) {
+    const element = i;
+    if(element== 5){
+        console.log("detected 5 ");
+        continue;
+        // break;
+        
+    }
+    console.log(element);
+}
+    // break
+for (let i = 0; i <= 20; i++) {
+    const element = i;
+    if(element== 5){
+        console.log("detected 5 ");
+        break;
+        
+    }
+    console.log(element);  
+}
+
